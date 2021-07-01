fef8eb73efd3207dac3784f3911bc44326582309
Doe Mohamud <doe.mahamud1@gmail.com>
2021-07-01 16:51:32 -0400

    commit tree 83714b52bdceb04d722e35e816637aefac9fe0a7

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Moe"
+    end
+
+    get '/hometown' do
+        "My hometown is Lewiston"
+    end
+
+    get '/favorite-song' do     
+        "My favorite song is 1hunnit"
+    end
+    
 end
